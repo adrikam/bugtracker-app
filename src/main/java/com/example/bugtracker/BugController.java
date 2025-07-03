c3d4a045c4c04374553f3ed4417af3649bf5bd75
adrikam <2205872@kiit.ac.in>
2025-07-04 02:10:27 +0530

    Fix: added CrossOrigin for Netlify frontend

--- src/main/java/com/example/bugtracker/BugController.java
+++ src/main/java/com/example/bugtracker/BugController.java
@@ -1,11 +1,19 @@
 package com.example.bugtracker;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "https://heroic-yeot-5cbd71.netlify.app")
 @RestController
 @RequestMapping("/api/bugs")
 public class BugController {
